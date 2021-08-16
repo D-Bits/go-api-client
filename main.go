cbce84ee0fdea7f7b589ec9411953544d9628c36
D-Bits <lockwood.da88@gmail.com>
2021-08-15 17:37:32 -0700

    Added stdin logic.

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
 
-	getData("https://danabases.net/data/people.json", "people")
+	// Create a reader
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter a URL: ")
+	url, _ := reader.ReadString('\n')
+	// Remove the newline before passing into getData() function
+	formattedUrl := strings.TrimSuffix(url, "\n")
+	fmt.Print("Enter a file name: ")
+	filename, _ := reader.ReadString('\n')
+	formattedFilename := strings.TrimSuffix(filename, "\n")
+
+	getData(formattedUrl, formattedFilename)
 
 }
